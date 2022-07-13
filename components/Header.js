b6dea64dfceac875cd39f8511eb617e067ff0e1d
putraperdana138 <80741806+putraprdn@users.noreply.github.com>
2022-07-13 10:27:41 +0700

    feat(components): build header component

---
+++ components/Header.js
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+function Header() {
+	return (
+		<header className="">
+			<div>
+            </div>
+			<Image
+				className="object-contain"
+				src="https://links.papareact.com/ua6"
+				width={200}
+				height={100}
+			/>
+		</header>
+	);
+}
+
+export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
+import Header from "../components/Header";
 
 export default function Home() {
 	return (
@@ -12,7 +13,13 @@ export default function Home() {
 				/>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
-			<h1>Let's build yahu</h1>
+
+			{/* Header */}
+			<Header />
+
+			{/* Nav */}
+
+			{/* Results */}
 		</div>
 	);
 }
