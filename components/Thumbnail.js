3bdd14c750b526314e5bb50198f3bb33144a8e5f
putraperdana138 <80741806+putraprdn@users.noreply.github.com>
2022-07-13 17:06:02 +0700

    feat(component): create result component

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,11 +1,15 @@
 import { ThumbUpIcon } from "@heroicons/react/outline";
 import Image from "next/image";
+import { forwardRef } from "react";
 
-function Thumbnail({ result }) {
+const Thumbnail = forwardRef(({ result }, ref) => {
 	const BASE_URL = "https://image.tmdb.org/t/p/original/";
 
 	return (
-		<div className="p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
+		<div
+			ref={ref}
+			className="p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50"
+		>
 			<Image
 				layout="responsive"
 				src={
@@ -25,13 +29,12 @@ function Thumbnail({ result }) {
 				</h2>
 
 				<p className="flex items-center opacity-0 group-hover:opacity-100">
-					{result.media_type && `${result.media_type} - `}{" "}
 					{result.release_date || result.first_air_date} |{" "}
 					<ThumbUpIcon className="h-5 mx-2" /> {result.vote_count}
 				</p>
 			</div>
 		</div>
 	);
-}
+});
 
 export default Thumbnail;
